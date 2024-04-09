52c6b7a002217d0068efef480d410e79c457bf81
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-04-09 23:09:39 +0200

    first commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,24 @@
+const MODEL_PATH = 'https://storage.googleapis.com/jmstore/TensorFlowJS/EdX/SavedModels/sqftToPropertyPrice/model.json'
+let model = undefined
+
+async function loadModel() {
+  model = await tf.loadLayersModel(MODEL_PATH)
+  model.summary()
+
+  const input = tf.tensor2d([[870]])
+  const inputBatch = tf.tensor2d([[500], [1100], [970]])
+
+  const result = model.predict(input)
+  const resultBatch = model.predict(inputBatch)
+
+  result.print()
+  resultBatch.print()
+
+  input.dispose()
+  inputBatch.dispose()
+  result.dispose()
+  resultBatch.dispose()
+  model.dispose()
+}
+
+loadModel()
